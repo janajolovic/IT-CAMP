c8bd70ebd8f5f4960bbe5719e65c0bdb83589dae
Jana Jolovic <janajolovic04@gmail.com>
2021-07-12 22:50:03 +0200

    some changes

--- IT CAMP/forma/forma.js
+++ IT CAMP/forma/forma.js
@@ -1,1 +1,15 @@
-
+function changeUsername() {
+    document.getElementById("usernameVal").innerHTML = "username: " + document.getElementById("username").value;
+}
+function changePassword() {
+    document.getElementById("passwordVal").innerHTML = "password: " + document.getElementById("password").value;
+}
+function changeSelect() {
+    document.getElementById("selectVal").innerHTML = "select: " + document.getElementById("select").value;
+}
+function changeRange() {
+    document.getElementById("rangeVal").innerHTML = "range: " + document.getElementById("range").value;
+}
+function changeDatetime() {
+    document.getElementById("datetimeVal").innerHTML = "datetime: " + document.getElementById("datetime").value;
+}
