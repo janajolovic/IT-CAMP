12c48068c791867af86200d5ae1756c8047d7440
Jana Jolovic <janajolovic04@gmail.com>
2021-08-28 22:36:58 +0200

    added some files

--- ITCAMP/vezba2.js
+++ ITCAMP/vezba2.js
@@ -23,37 +23,62 @@
 
 
 
-L = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"];
-M = ["A", "B", "C", "W"]
+// L = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"];
+// M = ["A", "B", "C", "W"]
 
 
-function stockList(listOfArt, listOfCat){
-    if (listOfCat.length === 0 || listOfArt.length === 0) {
-        return ""
-    }
-    res = [];
+// function stockList(listOfArt, listOfCat){
+//     if (listOfCat.length === 0 || listOfArt.length === 0) {
+//         return ""
+//     }
+//     res = [];
+//     arr = []
+//     for (i=0; i<listOfCat.length; i++) {
+//         arr.push(listOfArt[i].split(" "))
+//     }
+
+//     for (let i = 0; i < listOfCat.length; i++) {
+//         num = 0;
+//         for (let j = 0; j <arr.length; j++) {
+//             if (listOfCat[i] === arr[j][0][0]) {
+//                 num += parseInt(arr[j][1])
+//             }
+//         }
+//         res.push(`(${listOfCat[i]} : ${num})`)
+//     }
+//     res = res.join(" - ");
+//     return res
+// }
+
+
+// console.log(stockList(L,M));
+
+
+
+
+
+
+str = 'man i need a taxi up to ubud'
+alp = "abcdefghijklmnopqrstuvwxyz"
+
+function high(str) {
     arr = []
-    for (i=0; i<listOfCat.length; i++) {
-        arr.push(listOfArt[i].split(" "))
-    }
-
-    for (let i = 0; i < listOfCat.length; i++) {
-        num = 0;
-        for (let j = 0; j <arr.length; j++) {
-            if (listOfCat[i] === arr[j][0][0]) {
-                num += parseInt(arr[j][1])
+    str = str.split(" ");
+    for (i=0; i<str.length; i++) {
+        word = 0
+        for(j=0; j<=str[i].length; j++) {
+            for (k=0; k<alp.length; k++) {
+                if (str[i][j] === alp[k]) {
+                    word += alp.indexOf(alp[k]) + 1
+                }
+                }
             }
+        arr.push({ word : str[i], points : word})
         }
-        res.push(`(${listOfCat[i]} : ${num})`)
-    }
-    res = res.join(" - ");
-    return res
+    arr.sort((a,b) => a.points - b.points)
+    
+    return arr[arr.length-1].word
 }
 
 
-console.log(stockList(L,M));
-
-
-
-
-
+console.log(high(str))
