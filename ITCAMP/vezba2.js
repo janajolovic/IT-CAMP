4ce378e635c76616c9675e48bad814b1ae10c880
Jana Jolovic <janajolovic04@gmail.com>
2021-08-30 22:06:38 +0200

    some changes

--- ITCAMP/vezba2.js
+++ ITCAMP/vezba2.js
@@ -58,27 +58,53 @@
 
 
 
-str = 'man i need a taxi up to ubud'
-alp = "abcdefghijklmnopqrstuvwxyz"
+// str = 'man i need a taxi up to ubud'
+// alp = "abcdefghijklmnopqrstuvwxyz"
 
-function high(str) {
-    arr = []
-    str = str.split(" ");
-    for (i=0; i<str.length; i++) {
-        word = 0
-        for(j=0; j<=str[i].length; j++) {
-            for (k=0; k<alp.length; k++) {
-                if (str[i][j] === alp[k]) {
-                    word += alp.indexOf(alp[k]) + 1
-                }
-                }
-            }
-        arr.push({ word : str[i], points : word})
-        }
-    arr.sort((a,b) => a.points - b.points)
+// function high(str) {
+//     arr = []
+//     str = str.split(" ");
+//     for (i=0; i<str.length; i++) {
+//         word = 0
+//         for(j=0; j<=str[i].length; j++) {
+//             for (k=0; k<alp.length; k++) {
+//                 if (str[i][j] === alp[k]) {
+//                     word += alp.indexOf(alp[k]) + 1
+//                 }
+//                 }
+//             }
+//         arr.push({ word : str[i], points : word})
+//         }
+//     arr.sort((a,b) => a.points - b.points)
     
-    return arr[arr.length-1].word
-}
+//     return arr[arr.length-1].word
+// }
 
 
-console.log(high(str))
+// console.log(high(str))
+
+
+
+
+
+// array = [NaN, 0, 15, false, -22, undefined, 47, null];
+
+
+// array = array.filter(el => !!el)
+
+// console.log(array)
+
+
+
+
+array1 = [1,0,2,3,4]
+array2 = [3,5,6,7,8,13]
+
+array = array2.map(function(el, indx, arr) {
+    if (this[indx]) {
+        return el + this[indx];
+    } else return el;
+}, array1);
+
+
+console.log(array)
