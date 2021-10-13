99ae5dd462fd88c430ee8a3c6f6c84b4f946daa1
Jana Jolovic <janajolovic04@gmail.com>
2021-10-13 23:30:37 +0200

    some changes

--- ITCAMP/randomUser/app.js
+++ ITCAMP/randomUser/app.js
@@ -1,20 +1,27 @@
-function createCard(text) {
+function createCard(name, email, phone) {
     const container = document.querySelector(".container");
     const card = document.createElement("div");
-    const title = document.createElement("p");
-    title.innerHTML = text;
+    const Name = document.createElement("p");
+    const Email = document.createElement("p");
+    const Phone = document.createElement("p");
+    Name.innerHTML = `Name: ${name}`;
+    Email.innerHTML = `Email: ${email}`;
+    Phone.innerHTML = `Phone: ${phone}`;
     card.classList.add("card");
     container.appendChild(card);
-    card.appendChild(title);
-    console.log(title)
+    card.appendChild(Name);
+    card.appendChild(Email);
+    card.appendChild(Phone);
 }
 
 async function getData() {
     let response = await fetch(`https://randomuser.me/api`)
     if (response.status === 200) {
         response = await response.json();
-        console.log(response.results[0].name.first)
-        createCard(response.results[0].name.first)
+        const name = response.results[0].name.first + ' ' + response.results[0].name.last
+        const email = response.results[0].email;
+        const phone = response.results[0].phone;
+        createCard(name, email, phone);
     } 
 }
 
