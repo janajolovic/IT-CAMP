ba98afde362ee58c7b0f2c486a640f251a727701
Jana Jolovic <janajolovic04@gmail.com>
2021-10-13 23:41:04 +0200

    some changes

--- ITCAMP/randomUser/app.js
+++ ITCAMP/randomUser/app.js
@@ -1,14 +1,17 @@
-function createCard(name, email, phone) {
+function createCard(name, email, phone, image) {
     const container = document.querySelector(".container");
     const card = document.createElement("div");
     const Name = document.createElement("p");
     const Email = document.createElement("p");
     const Phone = document.createElement("p");
+    const Image = document.createElement("img");
     Name.innerHTML = `Name: ${name}`;
     Email.innerHTML = `Email: ${email}`;
     Phone.innerHTML = `Phone: ${phone}`;
+    Image.src = image;
     card.classList.add("card");
     container.appendChild(card);
+    card.appendChild(Image)
     card.appendChild(Name);
     card.appendChild(Email);
     card.appendChild(Phone);
@@ -21,7 +24,8 @@ async function getData() {
         const name = response.results[0].name.first + ' ' + response.results[0].name.last
         const email = response.results[0].email;
         const phone = response.results[0].phone;
-        createCard(name, email, phone);
+        const image = response.results[0].picture.large;
+        createCard(name, email, phone, image);
     } 
 }
 
