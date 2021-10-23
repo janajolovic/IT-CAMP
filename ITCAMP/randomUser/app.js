61b481036a1e385a55456e4963d66c398299c10f
Jana Jolovic <janajolovic04@gmail.com>
2021-10-23 23:31:02 +0200

    some changes

--- ITCAMP/randomUser/app.js
+++ ITCAMP/randomUser/app.js
@@ -21,10 +21,11 @@ async function getData() {
     let response = await fetch(`https://randomuser.me/api`)
     if (response.status === 200) {
         response = await response.json();
-        const name = response.results[0].name.first + ' ' + response.results[0].name.last
-        const email = response.results[0].email;
-        const phone = response.results[0].phone;
-        const image = response.results[0].picture.large;
+        response = response.results[0];
+        const name = response.name.first + ' ' + response.name.last
+        const email = response.email;
+        const phone = response.phone;
+        const image = response.picture.large;
         createCard(name, email, phone, image);
     } 
 }
