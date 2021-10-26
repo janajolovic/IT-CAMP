5133fe3c94b2a819c831cade15b63706268b4eca
Jana Jolovic <janajolovic04@gmail.com>
2021-10-26 22:20:25 +0200

    pagination added css

--- ITCAMP/fetch/app.js
+++ ITCAMP/fetch/app.js
@@ -1,25 +1,9 @@
-
-// async function createCard() {
-//     fetch("https://catfact.ninja/fact")
-//         .then((response) => response.json())
-//         .then((data) => {
-//             let fact = data.fact;
-//             console.log(fact)
-//             const fact_card = document.createElement("div");
-//             fact_card.classList.add("fact-card");
-//             fact_card.innerHTML = `<p>${fact}</p>`;
-//             container.appendChild(fact_card); 
-//         })
-// }
-
-// for (i=0; i <= limit; i++) {
-//     createCard()
-// }
-
-
-
 const limit = 12;
 resData = null;
+let page = 1;
+const pages = document.querySelector(".pagination");
+const link = `https://catfact.ninja/facts?page=${page}&limit=${limit}`;
+
 
 function createCard(text) {
     const container = document.querySelector(".container");
@@ -32,11 +16,30 @@ function createCard(text) {
 }
 
 async function getData() {
-    let response = await fetch(`https://catfact.ninja/facts?limit=${limit}`)
+    let response = await fetch(link)
     if (response.status === 200) {
         response = await response.json();
+        console.log(response)
         response.data.forEach(element => createCard(element.fact))
+
+        const pg_num = response.current_page;
+        const last_page = response.last_page;
+
+        // pagination(pg_num, last_page)
     } 
 }
 
+
+// function pagination(page_num, last_page) {
+//     for (let i = 1; i <= last_page; i++) {
+//         const pg_num = document.createElement("a")
+//         pg_num.innerHTML = i;
+//         pages.appendChild(pg_num);
+//     }
+// }
+
+
 getData()
+
+
+
