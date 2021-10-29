a6ced9b5791a858cb2aed9669c1426ac52856494
Jana Jolovic <janajolovic04@gmail.com>
2021-10-29 19:15:05 +0200

    clear container

--- ITCAMP/fetch/app.js
+++ ITCAMP/fetch/app.js
@@ -1,13 +1,13 @@
 const limit = 12;
-resData = null;
 let page = 1;
 const pages = document.querySelector(".pagination");
+let container = document.querySelector(".container");
 let link = `https://catfact.ninja/facts?page=${page}&limit=${limit}`;
 const ul = document.querySelector("ul");
 const btn = document.querySelector(".submit_btn");
 
 function createCard(text) {
-    const container = document.querySelector(".container");
+    let container = document.querySelector(".container");
     const card = document.createElement("div");
     const title = document.createElement("p");
     title.innerHTML = text;
@@ -23,7 +23,6 @@ async function getData(link) {
         response.data.forEach(element => createCard(element.fact))
         const pg_num = response.current_page;
         const last_page = response.last_page;
-
         pagination(pg_num, last_page)
     } 
 }
@@ -36,12 +35,12 @@ function pagination(page, total) {
     link = `https://catfact.ninja/facts?page=${page}&limit=${limit}`;
 
     if (page > 1) {
-        li += `<li class="btn prev" onclick="pagination(${page-1}, ${total}), getData(link)">
+        li += `<li class="btn prev" onclick="Clear(), pagination(${page-1}, ${total}), getData(link)">
                 <span><i class="fas fa-angle-left"></i> prev</span></li>`;
     }
 
     if (page > 2) {
-        li += `<li class="num" onclick="pagination(1, ${total}), getData(link)"><span>1</span></li>`
+        li += `<li class="num" onclick="Clear(), pagination(1, ${total}), getData(link)"><span>1</span></li>`
         if (page > 3) {
             li += `<li class="dots"><span>...</span></li>`
         }
@@ -56,17 +55,18 @@ function pagination(page, total) {
         }
         if (i === page) {
             li += `<li class="num active"><span>${i}</span></li>`
-        } else li += `<li class="num" onclick="pagination(${i}, ${total}), getData(link)"><span>${i}</span></li>`
+        } else li += `<li class="num" onclick="Clear(), pagination(${i}, ${total}), getData(link)"><span>${i}</span></li>`
     }
     
     if (page < total-1) {
         if (page < total - 2) {
             li += `<li class="dots"><span>...</span></li>`
         }
-        li += `<li class="num" onclick="pagination(${total}, ${total}), getData(link)"><span>${total}</span></li>`
+        li += `<li class="num" onclick="Clear(), pagination(${total}, ${total}), getData(link)"><span>${total}</span></li>`
     }
     if (page < total) {
-        li += `<li class="btn next" onclick="pagination(${page+1}, ${total}), getData(link)"><span>next <i class="fas fa-angle-right"></i></span></li>`;
+        li += `<li class="btn next" onclick="Clear(), pagination(${page+1}, ${total}), getData(link)">
+                <span>next <i class="fas fa-angle-right"></i></span></li>`;
     }
     
     ul.innerHTML = li;
@@ -74,13 +74,18 @@ function pagination(page, total) {
     
 }
 
-btn.setAttribute("onclick", 'FindPage(28)');
+function Clear() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
+    }
+}
+
+btn.setAttribute("onclick", 'Clear(), FindPage(28)');
 
 function FindPage(total) {
     let input = document.querySelector(".pg_input").value;
     link = `https://catfact.ninja/facts?page=${input}&limit=${limit}`;
     page = input;
-    console.log(page, input, link, total)
     pagination(page, total)
     getData(link)
     input = '';
@@ -91,5 +96,3 @@ getData(link)
 
 // TODO
 // reset input value 
-// fix pagination on button
-// make func
