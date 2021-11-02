f347e28516b07a1e8432d2fa2fafe592b7ee1883
Jana Jolovic <janajolovic04@gmail.com>
2021-11-02 21:31:32 +0100

    some changes

--- ITCAMP/fetch/app.js
+++ ITCAMP/fetch/app.js
@@ -17,6 +17,7 @@ function createCard(text) {
 }
 
 async function getData(link) {
+    Clear()
     let response = await fetch(link)
     if (response.status === 200) {
         response = await response.json();
@@ -35,12 +36,12 @@ function pagination(page, total) {
     link = `https://catfact.ninja/facts?page=${page}&limit=${limit}`;
 
     if (page > 1) {
-        li += `<li class="btn prev" onclick="Clear(), pagination(${page-1}, ${total}), getData(link)">
+        li += `<li class="btn prev" onclick="pagination(${page-1}, ${total}), getData(link)">
                 <span><i class="fas fa-angle-left"></i> prev</span></li>`;
     }
 
     if (page > 2) {
-        li += `<li class="num" onclick="Clear(), pagination(1, ${total}), getData(link)"><span>1</span></li>`
+        li += `<li class="num" onclick="pagination(1, ${total}), getData(link)"><span>1</span></li>`
         if (page > 3) {
             li += `<li class="dots"><span>...</span></li>`
         }
@@ -55,17 +56,17 @@ function pagination(page, total) {
         }
         if (i === page) {
             li += `<li class="num active"><span>${i}</span></li>`
-        } else li += `<li class="num" onclick="Clear(), pagination(${i}, ${total}), getData(link)"><span>${i}</span></li>`
+        } else li += `<li class="num" onclick="pagination(${i}, ${total}), getData(link)"><span>${i}</span></li>`
     }
     
     if (page < total-1) {
         if (page < total - 2) {
             li += `<li class="dots"><span>...</span></li>`
         }
-        li += `<li class="num" onclick="Clear(), pagination(${total}, ${total}), getData(link)"><span>${total}</span></li>`
+        li += `<li class="num" onclick="pagination(${total}, ${total}), getData(link)"><span>${total}</span></li>`
     }
     if (page < total) {
-        li += `<li class="btn next" onclick="Clear(), pagination(${page+1}, ${total}), getData(link)">
+        li += `<li class="btn next" onclick="pagination(${page+1}, ${total}), getData(link)">
                 <span>next <i class="fas fa-angle-right"></i></span></li>`;
     }
     
@@ -74,11 +75,14 @@ function pagination(page, total) {
     
 }
 
+
 function Clear() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild)
-    }
+    // while (container.firstChild) {
+    //     container.removeChild(container.firstChild)
+    // }
+    container.innerHTML = ""
 }
+
 
 function FindPage(total) {
     let input = document.querySelector(".pg_input").value;
