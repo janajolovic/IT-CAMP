38c44ef4ddd253b523b2c9ad85725818e70dc228
Jana Jolovic <janajolovic04@gmail.com>
2021-11-14 13:14:37 +0100

    closures examples

---
+++ ITCAMP/closureExamples.js
@@ -0,0 +1,57 @@
+// HOISTING
+
+
+// console.log(num); // Returns 'undefined' from hoisted var declaration (not 6)
+// var num; // Declaration
+// num = 6; // Initialization
+// console.log(num); // Returns 6 after the line with initialization is executed.
+
+
+
+// The same thing happens if we declare and initialize the variable in the same line.
+
+// console.log(num); // Returns 'undefined' from hoisted var declaration (not 6)
+// var num = 6; // Initialization and declaration.
+// console.log(num); // Returns 6 after the line with initialization is executed.
+
+
+
+// CLOSURES
+
+// function init() {
+//     var name = 'Mozilla'; // name is a local variable created by init
+//     function displayName() { // displayName() is the inner function, a closure
+//       console.log(name); // use variable declared in the parent function
+//     }
+//     displayName();
+//   }
+//   init();
+
+
+
+// function makeFunc() {
+//     var name = 'Mozilla';
+//     function displayName() {
+//       console.log(name);
+//     }
+//     return displayName;
+// }
+
+// var myFunc = makeFunc();
+// myFunc();
+
+
+
+
+
+function makeAdder(x) {
+    return function(y) {
+      return x + y;
+    };
+  }
+  
+  var add5 = makeAdder(5);
+  var add10 = makeAdder(10);
+  
+  console.log(add5(2));  // 7
+  console.log(add10(2)); // 12
