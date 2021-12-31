2bfaa3d8c58a6422829b79bbe9f186fa7e42f94e
Jana Jolovic <janajolovic04@gmail.com>
2021-12-31 16:54:37 +0100

    delete btn

--- ITCAMP/REACT/react1/src/components/Todo.jsx
+++ ITCAMP/REACT/react1/src/components/Todo.jsx
@@ -4,6 +4,7 @@ const Todo = ({text}) => {
     return (
         <div className='todo-item'>
             <h1>{text}</h1>
+            <button className='delete-btn'><i class="fas fa-times"></i></button>
         </div>
     )
 }
